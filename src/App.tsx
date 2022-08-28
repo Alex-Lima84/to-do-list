d74981663a9d710e26ebe00068d32735b81627c3
Alexandre Luiz <alexandre.cerutti@live.com>
2022-08-28 15:43:35 -0300

    implementations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Footer } from './components/footer';
 import { Header } from './components/header';
 import styles from './App.module.css'
@@ -7,17 +7,39 @@ import { TaskList } from './components/task-list';
 import { ITask } from './interfaces';
 
 function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([])
+
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter(task => {
+        return task.id !== id
+      })
+    )
+  }
+
   return (
     <div>
       <Header />
       <main className={styles.main}>
+
         <div>
+
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText='Criar tarefa'/>
+          <TaskForm btnText='Criar tarefa'
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
+
         </div>
+
         <div>
+
           <h2>Suas tarefas:</h2>
-          <TaskList />
+          <TaskList taskList={taskList}
+            handleDelete={deleteTask}
+          />
+
         </div>
 
       </main>
--- src/components/task-form/index.tsx
+++ src/components/task-form/index.tsx
@@ -4,51 +4,63 @@ import { ITask } from '../../interfaces'
 
 interface IProps {
   btnText: string
+  taskList: ITask[]
+  setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>> // Esta tipagem informa que estamos alterando o estado de uma lista, neste caso a lista ITask.
 }
 
-export const TaskForm = ({ btnText }: IProps) => {
+export const TaskForm = ({ btnText, taskList, setTaskList }: IProps) => {
 
   const [id, setId] = useState<number>(0)
   const [title, setTitle] = useState<string>('')
   const [difficulty, setDifficulty] = useState<number>(0)
 
-  const addTaskHandler = () => {
+  const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
 
+    e.preventDefault()
+
+    const id = Math.floor(Math.random() * 1000)
+    const newTask: ITask = { id, title, difficulty }
+
+    setTaskList!([...taskList, newTask])
+    setTitle('')
+    setDifficulty(0)
   }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if(e.target.name === 'title') {
+    if (e.target.name === 'title') {
       setTitle(e.target.value)
     }
-    if(e.target.name === 'difficulty') {
+    if (e.target.name === 'difficulty') {
       setDifficulty(parseInt(e.target.value))
     }
-    console.log(title)
-    console.log(difficulty)
   }
 
 
   return (
-    <form onSubmit={addTaskHandler} 
-          className={styles.form}>
+    <form onSubmit={addTaskHandler}
+      className={styles.form}>
 
       <div className={styles.input_container}>
 
         <label htmlFor='title'>Título:</label>
-        <input  onChange={handleChange} 
-                type='text' 
-                name='title' 
-                placeholder='Título da tarefa' />
+        <input onChange={handleChange}
+          type='text'
+          name='title'
+          placeholder='Título da tarefa'
+          value={title}
+        />
 
       </div>
 
       <div className={styles.input_container}>
 
         <label htmlFor='difficulty'>Dificuldade:</label>
-        <input  onChange={handleChange} 
-                type='text' 
-                name='difficulty' 
-                placeholder='Dificuldade da tarefa' />
+        <input onChange={handleChange}
+          type='text'
+          name='difficulty'
+          placeholder='Dificuldade da tarefa'
+          value={difficulty}
+        />
 
       </div>
 
